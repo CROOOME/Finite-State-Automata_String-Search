f55d329f0c8b9ac8fd1a546a75ccade5a716c682
Marco Sandoval <marcosandoval68@gmail.com>
2020-08-10 21:35:56 -0700

    adding the basic pattern test

--- test_search.py
+++ test_search.py
@@ -5,9 +5,14 @@ import search
 class SearchTestCase(unittest.TestCase):
     def test_basic_pattern(self):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+        sequence = 'Does this string contain the string test?'
+        subsequence = 'test'
+
+        fsa = search.FSA(pattern=subsequence)
+        fsa.path()
+        response = fsa.search(sequence)
+
+        self.assertEqual(response, True)
 
 
 if __name__ == '__main__':
