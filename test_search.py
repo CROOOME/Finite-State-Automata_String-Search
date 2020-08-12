5135749d8fc82ad54b50264a91f73664ecdf0f5a
Marco Sandoval <marcosandoval68@gmail.com>
2020-08-11 21:20:00 -0700

    adding basic negate test

--- test_search.py
+++ test_search.py
@@ -14,6 +14,16 @@ class SearchTestCase(unittest.TestCase):
 
         self.assertEqual(response, True)
 
+    def test_basic_pattern_negative(self):
+        sequence = 'Does this string contain the string test?'
+        subsequence = 'water'
+
+        fsa = search.FSA(pattern=subsequence)
+        fsa.path()
+        response = fsa.search(sequence)
+
+        self.assertEqual(response, False)
+
 
 if __name__ == '__main__':
     unittest.main()
