a9e7f1d736ffc40b76c83b84efe22f1581438936
Marco Sandoval <marcosandoval68@gmail.com>
2020-08-11 21:22:15 -0700

    adding basic test that queries the front of the string

--- test_search.py
+++ test_search.py
@@ -14,6 +14,16 @@ class SearchTestCase(unittest.TestCase):
 
         self.assertEqual(response, True)
 
+    def test_basic_pattern_positive_front(self):
+        sequence = 'Does this string contain the string test?'
+        subsequence = 'Does'
+
+        fsa = search.FSA(pattern=subsequence)
+        fsa.path()
+        response = fsa.search(sequence)
+
+        self.assertEqual(response, True)
+
     def test_basic_pattern_negative(self):
         sequence = 'Does this string contain the string test?'
         subsequence = 'water'
